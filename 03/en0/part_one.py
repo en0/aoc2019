cdafe3bb5f32faf922f5cd9f6307b346fb8d3f64
Ian Laird <irlaird@gmail.com>
2019-12-03 19:19:47 -0700

    Added my solutions for 03

--- 03/en0/part_one.py
+++ 03/en0/part_one.py
@@ -1,9 +1,51 @@
 from sys import argv
 
 
-def main(resource_path):
-    raise NotImplementedError()
+class Journey(set):
 
+    def __init__(self):
+        super(Journey, self).__init__()
+        self._loc = (0, 0)
+
+    def walk(self, movement):
+        self._walk(movement[0], int(movement[1:]))
+
+    def _walk(self, direction, steps):
+        for i in range(steps):
+            self._step(direction)
+
+    def _step(self, direction):
+        x, y = self._loc
+        if direction == "U":
+            self.set_location((x, y + 1))
+        elif direction == "D":
+            self.set_location((x, y - 1))
+        elif direction == "L":
+            self.set_location((x - 1, y))
+        elif direction == "R":
+            self.set_location((x + 1, y))
+
+    def set_location(self, point: tuple):
+        self._loc = point
+        self.add(point)
+
+    def get_location(self) -> tuple:
+        return self._loc
+
+    @classmethod
+    def loads(cls, movements: str) -> "Journey":
+        ret = cls()
+        for movement in movements.split(","):
+            ret.walk(movement)
+        return ret;
+
+
+def main(resource_path):
+    with open(resource_path, 'r') as fd:
+        r = Journey.loads(fd.readline())
+        l = Journey.loads(fd.readline())
+        ans = sorted([abs(x)+abs(y) for x, y in r & l])[0]
+        print(ans)
 
 if __name__ == "__main__":
     main(argv[-1])
--- 03/en0/part_two.py
+++ 03/en0/part_two.py
@@ -1,9 +1,31 @@
 from sys import argv
+from part_one import Journey
+
+
+class TrackedJourney(Journey):
+
+    def __init__(self):
+        super(TrackedJourney, self).__init__()
+        self._step_count = 0
+        self._tracks = dict()
+
+    def step_count_at(self, point: tuple) -> int:
+        return self._tracks.get(point)
+
+    def set_location(self, point: tuple):
+        super(TrackedJourney, self).set_location(point)
+        self._step_count += 1
+        if point not in self._tracks:
+            self._tracks[point] = self._step_count
 
 
 def main(resource_path):
-    raise NotImplementedError()
-
+    with open(resource_path, 'r') as fd:
+        r = TrackedJourney.loads(fd.readline())
+        l = TrackedJourney.loads(fd.readline())
+        ans = min([r.step_count_at(loc) + l.step_count_at(loc)
+            for loc in r & l])
+        print(ans)
 
 if __name__ == "__main__":
     main(argv[-1])
