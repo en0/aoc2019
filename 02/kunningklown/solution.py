542eacfe5a80d5d22b4ecb9e418e055a7adf6bf4
Alan Kelsey <akelsey@Alans-MacBook-Pro.local>
2019-12-06 23:40:00 -0700

    doh finally got it

--- 02/kunningklown/solution.py
+++ 02/kunningklown/solution.py
@@ -38,39 +38,41 @@ Find the input noun and verb that cause the program to produce the output 196907
  
 data = []
 desired_ouput = 19690720
- 
+output = []
+data_init = []
  
 def set_opcode(op1: list):
-   # print(op1)
-   value_1_position = op1[1]
-   value_2_position = op1[2]
-   result_position = op1[3]
-   global data
-   global output
+    # print(op1)
+    value_1_position = op1[1]
+    value_2_position = op1[2]
+    result_position = op1[3]
+    global data
+    global output
  
-   if op1[0] == 1:
-       data[result_position] = int(data[value_1_position]) + int(data[value_2_position])
-       # print(f"set op1 {op1}: {int(data[value_1_position]) + int(data[value_2_position])}: {data[result_position]}")
-   if op1[0] == 2:
-       try:
-           data[result_position] = int(data[value_1_position]) * int(data[value_2_position])
-           # print(f"set op1 {op1}: {int(data[value_1_position]) * int(data[value_2_position])}: {data[result_position]}")
-       except IndexError as e:
-           print(e,op1)
-   if op1[0] > 2:
-       print(f"\nerror {op1}\n")
-       # print(f"{data}")
-       # quit()
-   print(data)
- 
- 
+    if op1[0] == 1:
+        data[result_position] = int(data[value_1_position]) + int(data[value_2_position])
+        # print(f"set op1 {op1}: {int(data[value_1_position]) + int(data[value_2_position])}: {data[result_position]}")
+    if op1[0] == 2:
+        try:
+            data[result_position] = int(data[value_1_position]) * int(data[value_2_position])
+            # print(f"set op1 {op1}: {int(data[value_1_position]) * int(data[value_2_position])}: {data[result_position]}")
+        except IndexError as e:
+            # print(e,op1)
+            pass
+    if op1[0] > 2:
+        # print(f"\nerror {op1}\n")
+        # print(f"{data}")
+        # quit()
+        # print(data)
+        pass
+
 def process_data():
-   global data
-   global output
-   output = data
-   temp_opcode = []
+    global data
+    global output
+    output = data
+    temp_opcode = []
  
-   for i in range(len(data)):
+    for i in range(len(data)):
        if len(temp_opcode) == 4:
            # print(f"length is 4")
            set_opcode(temp_opcode)
@@ -87,10 +89,16 @@ def set_data(new_data: list):
 def update_data(position: int, value: int):
    global data
    data[position] = value
+
+def reset_data():
+    global data
+    data = data_init
  
 def get_data_from_file():
-   with open('sample_resource.txt', 'r') as input_data:
+    global data_init
+    with open('sample_resource.txt', 'r') as input_data:
        set_data(input_data.read().split(","))
+       data_init = input_data.read().split(",")
  
  
 def brute():
@@ -98,23 +106,25 @@ def brute():
    pos_1 = 0 # noun
    pos_2 = 0 # verb
  
-   for pos_1_guess in range(0,100):
-       data[1] = pos_1_guess
-       for pos_2_guess in range(0,100):
+   for pos_1_guess in range(0, 100):
+       for pos_2_guess in range(0 ,100):
+           get_data_from_file() # OMG this was in the loop above - caused the issue :)
+           data[1] = pos_1_guess
            data[2] = pos_2_guess
-           print(f"pos 1: {pos_1_guess} pos 2: {pos_2_guess}\n {data}")
+           print(f"pos 1: {pos_1_guess} pos 2: {pos_2_guess}")
            process_data()
            if data[0] == desired_ouput:
-               print(f"Found match with {pos_1_guess} and {pos_2_guess}")
+               print(f"Found match with {pos_1_guess} and {pos_2_guess} answer is {100 * pos_1_guess + pos_2_guess}")
                quit()
            else:
-               print(f"value {data[0]} {pos_1_guess} and {pos_2_guess}")
-               get_data_from_file()
+            # print(f"value {data[0]} {pos_1_guess} and {pos_2_guess}")
+                print(f"value {data[0]},{data[1]},{data[2]},{data[3]}")
+                #  get_data_from_file()
+                reset_data()
        get_data_from_file()
  
 get_data_from_file()
 # process_data()
  
-# print(f"{data}")
+# print(f"{data[0]}")
 brute()
-
