9d4d17fef3f5dc8eceeef6e03057561196313e6e
Alan Kelsey <akelsey@Alans-MacBook-Pro.local>
2019-12-15 21:44:32 -0700

    well

--- 03/kunningklown/solution.py
+++ 03/kunningklown/solution.py
@@ -1,79 +1,110 @@
+'''
+read file
+split data on line return file_line1 file_line2
+split data on comma for both lines
+iterate through each instruction for each line to create wire1 wire2
+if points go from (0,0) to (0,3) line = (0,0),(0,1),(0,2),(0,3)
+if r,l,u,d add number
+
+'''
+
 class TraceCircuit():
     def __init__(self):
         self._x = 0
         self._y = 0
         self._cords = []
         self._steps = []
-        self._wire_1 = []
-        self._wire_2 = []
-        self._wires = []
+        self._file_line_1 = []
+        self._file_line_2 = []
+        self._file_data = []
         self._get_data_from_file()
         self._set_cords()
 
     def _up(self, count: str):
         # print(f"{self._y} added to {count} is {self._y + int(count)}")
-        self._y += int(count)
-        self._set_cords()
+        # self._y += int(count)
+        self.lines_from_instruction("y", int(count))
+        # self._set_cords()
 
     def _down(self, count: str):
         # print(f"{self._y} subtract {count} is {self._y - int(count)}")
-        self._y -= int(count)
-        self._set_cords()
+        self.lines_from_instruction("y", int(count))
+        # self._y -= int(count)
+        # self._set_cords()
 
     def _left(self, count: str):
         # print(f"{self._x} subtract {count} is {self._x - int(count)}")
-        self._x -= int(count)
-        self._set_cords()
+        self.lines_from_instruction("x", int(count))
+        # self._x -= int(count)
+        # self._set_cords()
 
     def _right(self, count: str):
         # print(f"{self._x} added to {count} is {self._x + int(count)}")
-        self._x += int(count)
-        self._set_cords()
+        self.lines_from_instruction("x", int(count))
+        # self._x += int(count)
+        # self._set_cords()
 
     def _get_data_from_file(self):
         lines = []
         file_count = 0
-        with open('sample.txt', 'r') as input_data:
+        with open('sample2.txt', 'r') as input_data:
             parts = input_data.read().split("\n")
-            self._wire_1 = parts[0].split(",")
-            self._wires.append(self._wire_1)
-            self._wire_2 = parts[1].split(",")
-            self._wires.append(self._wire_2)
+            self._file_line_1 = parts[0].split(",")
+            self._file_data.append(self._file_line_1)
+            self._file_line_2 = parts[1].split(",")
+            self._file_data.append(self._file_line_2)
 
     def _set_cords(self):
         self._cords.append({"x": self._x, "y": self._y})
-        # print({"x": self._x, "y": self._y})
+        print({"x": self._x, "y": self._y})
 
     def _process_intcode(self, instruction: list):
-        count = 0
         for step in instruction:
-            # print(step)
+            count = int(step[1:])
+            print(f"{step} : ")
             if step.startswith("U"):
-                self._up(step[1:])
+                # self._up(step[1:])
+                self.lines_from_instruction("y", int(count))
             if step.startswith("D"):
-                self._down(step[1:])
+                # self._down(step[1:])
+                self.lines_from_instruction("y", int(count * -1))
             if step.startswith("L"):
-                self._left(step[1:])
+                # self._left(step[1:])
+                self.lines_from_instruction("x", int(count * -1))
             if step.startswith("R"):
-                self._right(step[1:])
+                # self._right(step[1:])
+                self.lines_from_instruction("x", int(count))
             # self._set_cords()
             # print(f"{step}: {self._cords}\n\n")
 
+    def lines_from_instruction(self, x_or_y: str, end_point: int):
+        line_parts = []
+        print(end_point)
+        for _ in range(end_point):
+            print(f"{_} : {end_point}: {x_or_y}")
+            if x_or_y == 'x':
+                # line_parts.append[x_or_y,self._y]
+                self._y = _
+                self._set_cords()
+            else:
+                self._x = _
+                self._set_cords()
+
     def get_list_of_points(self):
         point_set = []
-        # print(self._wire_1 == self._wire_2)
-        for wire in self._wires:
+        # print(self._file_line_1 == self._file_line_2)
+        for instruction in self._file_data:
             # print("start")
-            self._process_intcode(wire)
+            self._process_intcode(instruction)
             point_set.append(self._cords)
             self._cords = [{"x": 0, "y": 0}]
             self._x = 0
             self._y = 0
         # print(point_set[0] == point_set[1])
-            # self._process_intcode(self._wire_1)
+            # self._process_intcode(self._file_line_1)
             # point_set.append(self._cords)
             # self._cords = [{"x": 0, "y": 0}]
-            # self._process_intcode(self._wire_2)
+            # self._process_intcode(self._file_line_2)
             # point_set.append(self._cords)
         return point_set
 
@@ -93,23 +124,27 @@ class TraceCircuit():
 
 test = TraceCircuit()
 cord_set = test.get_list_of_points()
-# print(cord_set[0] == cord_set[1])
-# print(f"x1,y1,x2,y2\n")
+# print(len(cord_set[1]))
+
 point_index = 0
-for points1 in cord_set[0]:
-    # print(point_index)
-    for points2 in cord_set[1]:
+best_match = 0
+
+# for points1 in cord_set[0]:
+    # print(points1)
+    # for points2 in cord_set[1]:
+        # print(f"{points1} : {points2}")
         # print(f"{points1['x']},{points1['y']},{points2['x']},{points2['y']},")
-        # print(f"{points1['x'] == points2['x']} : {points1['x']} and {points2['x']} ")
-        if points1['x'] in range(points2['x']):
-            print(f"match {points1} : {points2}")
-            # x1_y1 =
-            # point_a = test.calc_mahatten([points1['x'], points1['y']])
-            # point_b = test.calc_mahatten([points2['x'], points2['y']])
-            # if point_a > point_b:
-            #     print(f"{point_a} is bigger than {point_b}")
-            # else:
-            #     print(f"{point_a} is smaller than {point_b}")
-            # print(f"{point_index}: match {points1} and {cord_set[1][point_index]['x']}")
-    point_index += 1
+        # print(f"{points1 == points2} : {points1['x']} and {points2['x']} ")
+        # if points1 == points2:
+        #     # print(f"match {points1} : {points2}")
+        #     dist1 = test.calc_mahatten( [points1['x'], points1['y']])
+        #     dist2 = test.calc_mahatten( [points2['x'], points2['y']])
+        #     if dist1 < dist2:
+        #         if dist1 < best_match:
+        #             best_match = dist1
+        #     else:
+        #         if dist2 < best_match:
+        #             best_match = dist2
+    # point_index += 1
+print(best_match)
 
